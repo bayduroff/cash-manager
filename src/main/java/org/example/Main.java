7049e191db9a1e472838c02066ee19f278977e2e
Ilya Baydurov <ibajdurov@vsegda-da.com>
2025-09-24 16:50:36 +0300

    [02] tests added, null checks added, Main class cleared

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,22 +2,5 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        CashManager cashManager = new CashManager();
-
-        //добавление 5 элементов
-        cashManager.addToCash("w1");
-        cashManager.addToCash("w2");
-        cashManager.addToCash("w3");
-        cashManager.addToCash("w4");
-        cashManager.addToCash("w5");
-        var cash = cashManager.getCash();
-
-        //добавление 6-го
-        cashManager.addToCash("w6");
-        cash = cashManager.getCash();
-
-        //добавление 7-го
-        cashManager.addToCash("w7");
-        cash = cashManager.getCash();
     }
 }
